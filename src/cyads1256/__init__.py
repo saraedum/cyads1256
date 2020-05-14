0cfdca8577d1da59fb23fdf7763280564463b71c
Julian Rüth <julian.rueth@fsfe.org>
2020-05-15 01:18:30 +0200

    Implement more convenient read/read_many interface
    that also allows to specify relative measurements as (pin0, pin1)

--- src/cyads1256/__init__.py
+++ src/cyads1256/__init__.py
@@ -1,5 +1,7 @@
 from _bcm2835 import ads_init, read_and_set_next_channel
 
+from collections.abc import Iterable
+
 class ADS1256:
     def __init__(self, datarate, buffered=False, gain=1):
         if buffered:
@@ -11,18 +13,27 @@ class ADS1256:
 
         ads_init(datarate, gain)
 
-    def read(self, channels, final_next_channel=None):
+    def read(self, channel, next_channel=None):
+        if next_channel is None:
+            next_channel = channel
+
+        channel = self._encode_channel(channel)
+        next_channel = self._encode_channel(next_channel)
+
+        if channel != self.next_channel:
+            if self.next_channel != -1:
+                # TODO: log a warning if this is not the initial call
+                raise Exception
+            self._read_and_set_next_channel(channel)
+        timestamp, raw = self._read_and_set_next_channel(next_channel)
+        return timestamp, raw / 1677721 / self.gain
+
+    def read_many(self, channels, next_channel=None):
         ret = []
-        if final_next_channel is None:
-            final_next_channel = channels[0]
-        for (channel, next_channel) in zip(channels,channels[1:]+[final_next_channel]):
-            if channel != self.next_channel:
-                if self.next_channel != -1:
-                    # TODO: log a warning if this is not the initial call
-                    raise Exception
-                self._read_and_set_next_channel(next_channel)
-            timestamp, raw = self._read_and_set_next_channel(next_channel)
-            ret.append((timestamp, raw/1677721/self.gain))
+        if next_channel is None:
+            next_channel = channels[0]
+        for (channel, next_channel) in zip(channels, channels[1:] + [next_channel]):
+            ret.append(self.read(channel, next_channel))
         return ret
 
     def _read_and_set_next_channel(self, next_channel):
@@ -32,7 +43,12 @@ class ADS1256:
         self.next_timestamp = time()
         return ret
 
-    def compute_channel(self, pin1, pin2=None):
+    def _encode_channel(self, channel):
+        if isinstance(channel, Iterable):
+            pin1, pin2 = channel
+        else:
+            pin1, pin2 = channel, None
+
         if pin1 is None:
             pin1 = 8
         if pin2 is None:
